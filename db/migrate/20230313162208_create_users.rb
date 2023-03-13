cedad41979e1bcc00da4eb184a055fb8c0633eeb
kevin <kevin@soucheyre.org>
2023-03-13 17:53:25 +0100

    Fix migration conflict between devise & user migration

--- db/migrate/20230313162208_create_users.rb
+++ db/migrate/20230313162208_create_users.rb
@@ -3,8 +3,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
     create_table :users do |t|
       t.string :first_name
       t.string :last_name
-      t.string :email
-      t.string :password
+      # t.string :email
+      # t.string :password
       t.string :street
 
       t.timestamps
