d5503ff345c81171087f65f1e9885b0249fe0c2c
kevin <kevin@soucheyre.org>
2023-03-15 11:46:56 +0100

    Create AddIndexsToCartTable

---
+++ db/migrate/20230315102813_add_indexs_to_item_table.rb
@@ -0,0 +1,6 @@
+class AddIndexsToItemTable < ActiveRecord::Migration[7.0]
+  def change
+    add_reference :cart, :user, foreign_key: true
+    add_reference :cart, :item, foreign_key: true
+  end
+end
