381fc9210bc31ccfe48125090fecc011c9beb3c6
kevin <kevin@soucheyre.org>
2023-03-15 11:47:49 +0100

    Delete AddIndexsToItemTable

--- db/migrate/20230315102813_add_indexs_to_item_table.rb
+++
@@ -1,6 +0,0 @@
-class AddIndexsToItemTable < ActiveRecord::Migration[7.0]
-  def change
-    add_reference :cart, :user, foreign_key: true
-    add_reference :cart, :item, foreign_key: true
-  end
-end
