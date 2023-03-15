8fd12457dd039b40ebff2d46f190ef7a5593be0c
kevin <kevin@soucheyre.org>
2023-03-15 12:03:45 +0100

    Index migration success, need to do delete an other migration not necessary

--- db/migrate/20230315104542_add_indexs_to_cart_table.rb
+++ db/migrate/20230315104542_add_indexs_to_cart_table.rb
@@ -1,6 +1,6 @@
 class AddIndexsToCartTable < ActiveRecord::Migration[7.0]
   def change
-    add_reference :cart, :user, foreign_key: true
-    add_reference :cart, :item, foreign_key: true
+    add_reference :carts, :user, foreign_key: true
+    add_reference :carts, :item, foreign_key: true
   end
 end
