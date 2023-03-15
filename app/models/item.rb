e3efe857a81587d056d369bdbe2219afcb5360ff
Ulrichvoitier <ulrichvoitier@yahoo.fr>
2023-03-15 12:34:02 +0100

    create table order and link to user and item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   has_many :carts
   has_many :users, through: :cart
+  has_many :orders
+  has_many :users, through: :order
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_many :carts
   has_many :items, through: :cart
+  has_many :orders
+  has_many :items, through: :order
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
