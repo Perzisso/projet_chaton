f4412ea6ad57c3b98f7ddcd8d93b14af16d04324
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-16 18:09:47 +0100

    changing relation to cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,9 @@
 class Cart < ApplicationRecord
+  has_many :orders
+  has_many :items, through: :orders
+
+  def total
+      orders.to_a.sum { |order| order.total }
+  end
 
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
+  belongs_to :cart
+
+  def total
+    item.price * quantity
+  end
+
 end
