b8adce0dcb5d3863f8a33938d9b8f9ec7293addf
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-16 18:23:26 +0100

    fixing seed to have empty cart and orders

--- db/seeds.rb
+++ db/seeds.rb
@@ -27,21 +27,3 @@ puts "User succes"
   )
 end
 puts "Item succes"
-
-# Créer des paniers
-10.times do
-  Cart.create!(
-    user_id: User.all.sample.id,
-    item_id: Item.all.sample.id
-  )
-end
-puts "Cart succes"
-# Créer des commandes
-10.times do
-  Order.create!(
-    user_id: User.all.sample.id,
-    item_id: Item.all.sample.id,
-    purchase_number: Faker::Alphanumeric.alpha(number: 10)
-  )
-end
-puts "Order succes"
