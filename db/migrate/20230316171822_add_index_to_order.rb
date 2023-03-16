83916080b226390167810ea223e965889cb5d166
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-16 18:20:33 +0100

    fixing migrations

--- db/migrate/20230316171822_add_index_to_order.rb
+++ db/migrate/20230316171822_add_index_to_order.rb
@@ -1,5 +1,5 @@
 class AddIndexToOrder < ActiveRecord::Migration[7.0]
   def change
-    add_reference :orders, :item, null: false, foreign_key: true
+    add_reference :orders, :cart, foreign_key: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_164629) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_171822) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -34,6 +34,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_164629) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "purchase_number"
+    t.integer "quantity"
+    t.bigint "cart_id"
+    t.index ["cart_id"], name: "index_orders_on_cart_id"
     t.index ["item_id"], name: "index_orders_on_item_id"
     t.index ["user_id"], name: "index_orders_on_user_id"
   end
@@ -53,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_164629) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "carts"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
 end
