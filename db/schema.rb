e7707a512cb0411ce7ba507c92ea0189e55bc344
Benjamin Dupaquier <dupaquier.benjamin@gmail.com>
2023-03-16 17:49:16 +0100

    fixed migration for db;migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_113839) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_164629) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "item_id"
-    t.index ["item_id"], name: "index_carts_on_item_id"
-    t.index ["user_id"], name: "index_carts_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -57,8 +53,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_113839) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "carts", "items"
-  add_foreign_key "carts", "users"
   add_foreign_key "orders", "items"
   add_foreign_key "orders", "users"
 end
